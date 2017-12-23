67aa597423d9e6d2efe416fecb966c3c41aa62c6
Yange Yin <yinyange16@126.com>
2017-12-23 19:24:53 +0800

    Add a script keeping result but not x and y

---
+++ main3.py
@@ -0,0 +1,12 @@
+def f(x, y):
+    return x+y
+
+def main():
+    x = 6
+    y = 8
+    result = f(x, y)
+    return result
+
+if __name__ == '__main__':
+    result = main()
+# if you don't want to expose x and y ,you can write like this
